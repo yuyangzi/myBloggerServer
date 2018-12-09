87117fa2c531f3e43369e96adb0ed25e843c58c0
wangyiming <wangyimig@icloud.com>
2018-12-09 18:21:56 +0800

    feat: 添加文章上传接口

---
+++ src/config/path.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+// 上传文件存放路径
+const UPLOAD_FILE_PATH = path.join(__dirname, '../../public/upload');
+
+module.exports = {
+  UPLOAD_FILE_PATH,
+};
+
--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
-const {connect, initAdmin, initSchema} = require('./database/index');
-const {middleWaresRouter} = require('./middlewares/router');
-const {PORT} = require('./config/env.config.js');
+const koaBody = require('koa-body');
+const {
+  connect,
+  initAdmin,
+  initSchema
+} = require('./database/index');
+const {
+  middleWaresRouter
+} = require('./middlewares/router');
+const {
+  PORT
+} = require('./config/env.config.js');
 
 (async () => {
   const app = new Koa();
   app.use(bodyParser());
+  app.use(koaBody({
+    multipart: true,
+    formidable: {
+      maxFileSize: 200 * 1024 * 1024    // 设置上传文件大小最大限制，默认2M
+    }
+  }));
   app.use(async (ctx, next) => {
     // the parsed body will store in ctx.request.body
     // if nothing was parsed, body will be an empty object {}
